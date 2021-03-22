879ea1f3f057a8aa85998c1ad9000b2ba94d6953
Sandeep Mehrotra <sandeep2.mehrotra@gmail.com>
2021-03-22 16:01:38 +0000

    Removed additional comment

--- internal/server.js
+++ internal/server.js
@@ -19,8 +19,7 @@ app.use(bodyParser.json());
 const mockEvents = {
     events: [
         { title: 'an event', id: 1, description: 'something really cool' },
-        { title: 'another event', id: 2, description: 'something even cooler' },
-        { title: 'more events', id: 3, description: 'something even more cooler' }
+        { title: 'another event', id: 2, description: 'something even cooler' }
     ]
 };
 
